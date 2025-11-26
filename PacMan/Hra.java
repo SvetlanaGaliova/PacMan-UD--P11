6b464186805e714292b555379bbc8be3b8f5cd28
Lukas Blahut <blahut7@stud.uniza.sk>
2025-11-26 16:17:01 +0100

    skuska ci vsetko funguje jak ma

---
+++ PacMan/Hra.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Hra here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hra {
+    /**
+     * Constructor for objects of class Hra
+     */
+    public Hra() {
+        // initialise instance variables
+    }
+}
