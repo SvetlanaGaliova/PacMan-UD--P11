efa1654e327aab5f56257ba5efd3e52ff595c6b5
MatejKomarc <komarc@stud.uniza.sk>
2025-12-02 14:38:27 +0100

    Trieda Hrac verzia 1 a enum Smer verzia 1

---
+++ PacMan/Smer.java
@@ -0,0 +1,13 @@
+
+/**
+ * Enum pre smery pohybu hraca
+ *
+ * @author Matej Komarc
+ * @version (version number or date here)
+ */
+public enum Smer {
+    DOLAVA,
+    DOPRAVA,
+    HORE,
+    DOLE;
+}
